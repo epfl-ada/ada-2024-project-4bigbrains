13a153a5827edc0df899c0bb2b60155566eb63a8
adrcleme <clementadrien23@gmail.com>
2024-11-10 17:44:56 +0100

    Increased cleaning df visibility

--- Cleaning.py
+++ Cleaning.py
@@ -5,30 +5,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 def format_date(date) :
+    # Only taking movie release year
     date = date[:4]
     return date
 
 
-def GetCleanedData():
-    PATH = 'src'
-    header = ['Wikipedia movie ID', 'Freebase movie ID', 'Movie name', 'Movie release date', 'Movie box office revenue', 'Movie runtime', 
-            'Movie languages', 'Movie country', 'Movie genres']
-    df = pd.read_csv(PATH + '/movie.metadata.tsv', sep='\t', names=header, skipinitialspace=True)
+def GetCleanedData(df):
+    df = df.copy()
     
+    # Dropping all missing values for Movie country, languages and genres
     df = df[(df['Movie country'] != "{}") & (df['Movie languages'] != "{}") & (df['Movie genres'] != "{}")]
 
+    #  Transforming dictionary-like string representations into simpler formats
     df['Movie country'] = df['Movie country'].apply(lambda x: list(ast.literal_eval(x).values())[0])
     df['Movie genres'] = df['Movie genres'].apply(lambda x: list(ast.literal_eval(x).values()))
     df['Movie languages'] = df['Movie languages'].apply(lambda x: list(ast.literal_eval(x).values()))
 
-    df_date_revenue_clean = df.dropna(subset=['Movie release date', 'Movie box office revenue'])
-    df_date_clean = df.dropna(subset=['Movie release date'])
-    df_date_revenue_runtime_clean = df.dropna(subset=['Movie release date', 'Movie box office revenue', 'Movie runtime'])
+    df_date_revenue_clean = df.dropna(subset=['Movie release date', 'Movie box office revenue']).copy()
+    df_date_clean = df.dropna(subset=['Movie release date']).copy()
+    df_date_revenue_runtime_clean = df.dropna(subset=['Movie release date', 'Movie box office revenue', 'Movie runtime']).copy()
 
-    df_date_revenue_clean['Movie release date'] = df_date_revenue_clean['Movie release date'].apply(format_date)
-    df_date_clean['Movie release date'] = df_date_clean['Movie release date'].apply(format_date)
-    df_date_revenue_runtime_clean['Movie release date'] = df_date_revenue_runtime_clean['Movie release date'].apply(format_date)
+    df_date_revenue_clean.loc[:, 'Movie release date'] = df_date_revenue_clean['Movie release date'].apply(format_date)
+    df_date_clean.loc[:, 'Movie release date'] = df_date_clean['Movie release date'].apply(format_date)
+    df_date_revenue_runtime_clean.loc[:, 'Movie release date'] = df_date_revenue_runtime_clean['Movie release date'].apply(format_date)
 
     return df_date_clean, df_date_revenue_clean, df_date_revenue_runtime_clean
 
