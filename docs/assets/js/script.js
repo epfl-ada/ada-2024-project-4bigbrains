92814a81b0e6582604998ef185397fa4eb686bf2
ines <ines.montero@epfl.ch>
2024-12-10 16:02:30 +0100

    site internet 1st try

---
+++ docs/assets/js/script.js
@@ -0,0 +1,8 @@
+// Example of a Plotly plot in script.js
+Plotly.newPlot('plot1', [{
+    x: [1, 2, 3, 4, 5],
+    y: [10, 11, 12, 13, 14],
+    type: 'scatter'
+}], {
+    title: 'Top Cinematic Representatives'
+});
