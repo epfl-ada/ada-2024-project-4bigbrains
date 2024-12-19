5403e962bbcafb9e12aaf9e8675498f3b5bf257e
ines <ines.montero@epfl.ch>
2024-12-19 19:26:07 +0100

    final maybe?

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -34,7 +34,7 @@ document.getElementById('answer-form2').addEventListener('submit', function(even
     const userAnswer = document.getElementById('user-answer2').value.trim();
     
     // Spécifier la bonne réponse (par exemple, "solution")
-    const correctAnswer = "81"; 
+    const correctAnswer = "17"; 
 
     // Vérifier la réponse
     const feedback2 = document.getElementById('feedback2');
@@ -66,7 +66,7 @@ document.getElementById('answer-form3').addEventListener('submit', function(even
     const userAnswer = document.getElementById('user-answer3').value.trim();
     
     // Spécifier la bonne réponse (par exemple, "solution")
-    const correctAnswer = "30"; 
+    const correctAnswer = "10"; 
 
     // Vérifier la réponse
     const feedback3 = document.getElementById('feedback3');
@@ -117,18 +117,92 @@ document.getElementById('answer-form4').addEventListener('submit', function(even
 });
 
 
-
-
 // Script for hint 4
 document.getElementById('hint4-text').addEventListener('click', function() {
     this.classList.toggle('unblurred'); 
 });
 
-// Example of a Plotly plot in script.js
-Plotly.newPlot('plot1', [{
-    x: [1, 2, 3, 4, 5],
-    y: [10, 11, 12, 13, 14],
-    type: 'scatter'
-}], {
-    title: 'Top Cinematic Representatives'
+
+// Script for the button of Clue 5
+document.getElementById('answer-form5').addEventListener('submit', function(event) {
+    // Empêcher le rechargement de la page par défaut
+    event.preventDefault();
+
+    // Récupérer la réponse de l'utilisateur
+    const userAnswer = document.getElementById('user-answer5').value.trim();
+    
+    // Spécifier la bonne réponse (par exemple, "solution")
+    const correctAnswer = "432"; 
+
+    // Vérifier la réponse
+    const feedback4 = document.getElementById('feedback5');
+    
+    // Check if the answer matches
+    if (userAnswer === "") {
+        feedback4.textContent = "Please enter a valid answer.";
+        feedback4.style.color = "red";
+    } else if (userAnswer === correctAnswer) {
+        feedback4.textContent = "Success! You've entered the correct answer. Keep it in mind until the end";
+        feedback4.style.color = "green";
+    } else {
+        feedback4.textContent = "Incorrect answer. Try again!";
+        feedback4.style.color = "red";
+    }
 });
+
+
+// Script for hint 5
+document.getElementById('hint5-text').addEventListener('click', function() {
+    this.classList.toggle('unblurred'); 
+});
+
+
+// Script for the button of final answer
+document.getElementById('answer-formfinal').addEventListener('submit', function(event) {
+    // Empêcher le rechargement de la page par défaut
+    event.preventDefault();
+
+    // Récupérer la réponse de l'utilisateur
+    const userAnswer = document.getElementById('user-answerfinal').value.trim();
+    
+    // Spécifier la bonne réponse (par exemple, "solution")
+    const correctAnswer = "USAIUKJAI 17 10 DRATHRROM 432"; 
+
+    // Vérifier la réponse
+    const feedbackfinal = document.getElementById('feedbackfinal');
+    
+    // Check if the answer matches
+    if (userAnswer === "") {
+        feedbackfinal.textContent = "Please enter a valid answer.";
+        feedbackfinal.style.color = "red";
+    } else if (userAnswer === correctAnswer) {
+        feedbackfinal.textContent = "";
+        successContainer.style.display = "block"; // Affiche l'image
+    } else {
+        feedbackfinal.textContent = "Incorrect answer. Try again!";
+        feedbackfinal.style.color = "red";
+    }
+});
+
+
+
+
+// Script for hint final answer
+document.getElementById('hintfinal-text').addEventListener('click', function() {
+    this.classList.toggle('unblurred'); 
+});
+
+
+// Script for final results
+function showSuccess() {
+    const successContainer = document.getElementById('successContainer');
+    successContainer.style.display = 'flex'; 
+}
+
+// Fonction pour masquer l'image et le texte
+function hideSuccess() {
+    const successContainer = document.getElementById('successContainer');
+    successContainer.style.display = 'none'; 
+}
+
+document.getElementById('successContainer').addEventListener('click', hideSuccess);
